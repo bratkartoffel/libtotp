e3e9b3e954dc9f96f4a1aad3ce20e86746c13749
Simon Frankenberger <simon@fraho.eu>
2023-03-14 08:16:52 +0100

    add editorconfig

--- src/main/java/eu/fraho/libs/totp/Totp.java
+++ src/main/java/eu/fraho/libs/totp/Totp.java
@@ -144,9 +144,9 @@ public class Totp {
         byte[] hash = mac.doFinal(counter);
         int offset = hash[hash.length - 1] & 0x0f;
         return ((hash[offset] & 0x7f) << 24
-                | (hash[offset + 1] & 0xff) << 16
-                | (hash[offset + 2] & 0xff) << 8
-                | (hash[offset + 3] & 0xff)) % tokenMask;
+            | (hash[offset + 1] & 0xff) << 16
+            | (hash[offset + 2] & 0xff) << 8
+            | (hash[offset + 3] & 0xff)) % tokenMask;
     }
 
     @Contract(pure = true, value = "_ -> new")
